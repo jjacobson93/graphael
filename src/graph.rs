e94ad0471ba4cbe6361a585d4960fdec21b724d0
Daniel Yule <daniel.yule@gmail.com>
2015-11-03 16:18:33 -0400

    Updated Graphael to work with Rust 1.0

--- src/graph.rs
+++ src/graph.rs
@@ -1,34 +1,36 @@
-#![allow(unused_imports)]
 #![allow(dead_code)]
 
-use collections::{HashMap, TreeMap};
-use std::{fmt, cmp, uint};
-use std::io::{File, IoResult};
-use serialize::{json, Decodable, Encodable};
+use std::collections::{BTreeMap};
+use std::collections::btree_map::{Entry};
+use std::{fmt, cmp};
+use std::fs::{File};
+use std::io::{Result, Read, Write};
+use std::string::ToString;
+use rustc_serialize::{Decodable};
+use rustc_serialize::json::{Json, ToJson, Decoder};
 
 /**************************/
 /*** Struct definitions ***/
 /**************************/
 
-#[deriving(Decodable, Encodable)]
+#[derive(RustcDecodable, PartialEq, Debug)]
 pub struct Node {
 	id: NodeIndex,
-    pub props: HashMap<~str, ~str>
+    pub props: BTreeMap<Box<String>, Box<String>>
 }
-
-#[deriving(Decodable, Encodable)]
+#[derive(RustcDecodable, Debug)]
 pub struct Edge {
-	pub labels: ~[~str]
+	pub labels: Vec<String>
 }
 
-#[deriving(Decodable, Encodable)]
+#[derive(RustcDecodable, Debug)]
 pub struct Graph {
-	pub nodes: HashMap<NodeIndex, Node>,
-	pub edges: HashMap<NodeIndex, HashMap<NodeIndex, Edge>>
+	pub nodes: BTreeMap<NodeIndex, Node>,
+	pub edges: BTreeMap<NodeIndex, BTreeMap<NodeIndex, Edge>>
 }
 
-#[deriving(Eq, Hash, TotalEq, Decodable, Encodable)]
-type NodeIndex = uint;
+//#[derive(Eq, Hash, TotalEq, RustcDecodable, RustcEncodable)]
+pub type NodeIndex = usize;
 
 /***********************/
 /*** Implementations ***/
@@ -37,32 +39,61 @@ type NodeIndex = uint;
 /*** Node ***/
 
 /// Used to give a string representation for Node
-impl fmt::Show for Node {
+impl fmt::Display for Node {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f.buf, "Node<id={}, props={}>", self.id, self.props)
+		write!(f, "Node<id={}, props=", self.id);
+		for (k, v) in self.props.iter() {
+			write!(f, "{}, {}", k, v);
+		}
+		write!(f, ">")
 	}
 }
 
 /// Used to convert a Node to JSON
-impl json::ToJson for Node {
-	fn to_json(&self) -> json::Json {
-		let mut d = box TreeMap::new();
+impl ToJson for Node {
+	fn to_json(&self) -> Json {
+		let mut d = BTreeMap::new();
+		let mut props = BTreeMap::new();
 		d.insert("id".to_owned(), self.id.to_json());
-		d.insert("props".to_owned(), self.props.to_json());
-		json::Object(d)
+		for (&ref k, &ref v) in self.props.iter() {
+			props.insert(*k.clone(), Json::String(*v.clone()));
+		}
+		d.insert("props".to_owned(), Json::Object(props));
+		Json::Object(d)
+	}
+}
+
+impl ToJson for Edge {
+	fn to_json(&self) -> Json {
+		let mut d = BTreeMap::new();
+		d.insert("labels".to_string(), Json::Array(self.labels.iter().map(|l| Json::String(l.to_string())).collect()));
+		Json::Object(d)
+	}
+}
+
+impl ToJson for Graph {
+	fn to_json(&self) -> Json {
+		let mut d = BTreeMap::new();
+		let mut edge_json_map = BTreeMap::new();
+		d.insert("nodes".to_string(), Json::Object(self.nodes.iter().map(|(i, n)| (i.to_string(), n.to_json())).collect()));
+		for (index, edge) in self.edges.iter() {
+			edge_json_map.insert(index.to_string(), Json::Object(edge.iter().map(|(i, n)| (i.to_string(), n.to_json())).collect()));
+		}
+		d.insert("edges".to_string(), Json::Object(edge_json_map));
+		Json::Object(d)
 	}
 }
 
 /*** Edge ***/
 
 /// Used to give a string representation for Edge
-impl fmt::Show for Edge {
+impl fmt::Display for Edge {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f.buf, "Edge<labels={}>", self.labels)
+		write!(f, "Edge<labels={}>", self.labels.join(","))
 	}
 }
 
-impl cmp::Eq for Edge {
+impl cmp::PartialEq for Edge {
 	fn eq(&self, other: &Edge) -> bool {
 		self.labels == other.labels
 	}
@@ -71,42 +102,41 @@ impl cmp::Eq for Edge {
 /*** Graph ***/
 
 /// Used to give a string representation for Graph
-impl fmt::Show for Graph {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f.buf, "Graph (\n\tnodes = {}\n\tedges = {}\n)", self.nodes, self.edges)
-	}
-}
+// impl fmt::Display for Graph {
+// 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+// 		write!(f.buf, "Graph (\n\tnodes = {}\n\tedges = {}\n)", self.nodes, self.edges)
+// 	}
+// }
 
 impl Graph {
 
-	/// Creates a new graph with empty HashMaps
+	/// Creates a new graph with empty BTreeMaps
 	pub fn new() -> Graph {
-		Graph { nodes: HashMap::new(), edges: HashMap::new() }
+		Graph { nodes: BTreeMap::new(), edges: BTreeMap::new() }
 	}
 
 	/// Encode and decode from file
 
-	pub fn read_from_file(name: ~str) -> Graph {
-		let contents = File::open(&Path::new(name)).read_to_str();
-		let string = match contents {
-			Ok(v) => v,
-			Err(v) => fail!("Failed to load the file: {}", v)
-		};
-		let jsonstring = match json::from_str(string) {
+	pub fn read_from_file(name: String) -> Graph {
+		let mut contents = String::new();
+		let mut file:File = File::open(name).unwrap();
+
+		file.read_to_string(&mut contents).unwrap();
+		let jsonstring = match Json::from_str(&contents) {
 			Ok(a) => a,
-			Err(e) => fail!("Error reading JSON string: {}", e)
+			Err(e) => panic!("Error reading JSON string: {}", e)
 		};
 
-		let mut decoder = json::Decoder::new(jsonstring);
+		let mut decoder = Decoder::new(jsonstring);
 		match Decodable::decode(&mut decoder) {
 			Ok(x) => x,
-			Err(e) => fail!("Could not decode to graph: {}", e)
+			Err(e) => panic!("Could not decode to graph: {}", e)
 		}
 	}
 
-	pub fn write_to_file(&self, name: &'static str) -> IoResult<()> {
-		let mut file = File::create(&Path::new(name));
-		file.write(json::Encoder::buffer_encode(&self).as_slice())
+	pub fn write_to_file(&self, name: &'static str) -> Result<usize> {
+		let mut file = try!(File::create(name));
+		file.write(self.to_json().to_string().into_bytes().as_slice())
 	}
 
 	/*******************/
@@ -118,41 +148,46 @@ impl Graph {
 		self.nodes.len() + 1
 	}
 
-	/// Adds a Node to the Graph with a vector of attributes and a HashMap of properties
-	pub fn add_node(&mut self, attr: ~[~str], props: HashMap<~str, ~str>) -> NodeIndex {
+	/// Adds a Node to the Graph with a vector of attributes and a BTreeMap of properties
+	pub fn add_node(&mut self, attr: &[&str], props: BTreeMap<Box<String>, Box<String>>) -> NodeIndex {
 		let idx = self.next_node_index();
 		self.nodes.insert(idx, Node { id: idx, props: props });
+		// TODO This is weird
 		for a in attr.iter() {
-			self.add_edge(idx, idx, a.to_owned());
+			self.add_edge(idx, idx, a.to_string());
 		}
 		idx
 	}
 
 	/// Removes a Node from the Graph given a NodeIndex
-	pub fn remove_node(&mut self, idx: NodeIndex) -> bool {
-		self.nodes.remove(&idx)
+	pub fn remove_node(&mut self, idx: NodeIndex) -> Option<Node> {
+		// TODO should also remove the attached edges
+		match self.nodes.remove(&idx) {
+			Some(n) => Some(n),
+			None => None
+		}
 	}
 
 
 	/// Get Nodes with a given attribute
-	pub fn nodes_with_attr<'a>(&'a self, attr: ~str) -> ~[&'a Node] {
-		self.nodes.values().filter(|node| 
-			match self.edges.find(&node.id) {
-				Some(map) => match map.find(&node.id) {
-					Some(edge) => edge.labels.contains(&attr),
-					None => false
-				},
-				None => false
+	pub fn nodes_with_attr<'a>(&'a self, attr: &String) -> Vec<&Node> {
+		self.nodes.values().filter(|node|
+			if let Some(e) = self.edges.get(&node.id) {
+				if let Some(edge) = e.get(&node.id) {
+					edge.labels.contains(attr)
+				} else {
+					false
+				}
+			}
+			else {
+				false
 			}).collect()
 	}
 
 	/// Get Nodes with a key-value pair
-	pub fn nodes_with_prop<'a>(&'a self, key: ~str, value: ~str) -> ~[&Node] {
+	pub fn nodes_with_prop<'a>(&'a self, key: &String, value: &String) -> Vec<&Node> {
 		self.nodes.values().filter(|node|
-			match node.props.find(&key) {
-				Some(v) => v == &value,
-				None => false,
-			}).collect()
+			node.props.iter().find(|&(k, v)| **k == *key && **v == *value ).is_some()).collect()
 	}
 
 	/*******************/
@@ -160,18 +195,21 @@ impl Graph {
 	/*******************/
 
 	/// Add an Edge to the graph from the source NodeIndex to the target NodeIndex with a label
-	pub fn add_edge<'a>(&'a mut self, source: NodeIndex, target: NodeIndex, label: ~str) -> &'a mut Edge {
-		let src_map = self.edges.find_or_insert(source, HashMap::new());
-		src_map.insert_or_update_with(target, 
-			Edge { labels: ~[label.clone()] },
-			|_k, v| if !v.labels.contains(&label) { v.labels.push(label.clone()) })
+	pub fn add_edge<'a>(&'a mut self, source: NodeIndex, target: NodeIndex, label: String) -> & mut Edge {
+		//map.entry(key).get().unwrap_or_else(|v| v.insert(default))
+		let src_map = self.edges.entry(source).or_insert(BTreeMap::new());
+		match src_map.entry(target) {
+			Entry::Vacant(e) => {e.insert(Edge { labels: vec![label] });},
+			Entry::Occupied(mut e) => if e.get().labels.contains(&label) { e.get_mut().labels.push(label); } else {()}
+		};
+		src_map.get_mut(&source).unwrap()
 	}
 
 	/// Get Edges with a given label
-	pub fn edges_with_label<'a>(&'a mut self, label: ~str) -> HashMap<&'a NodeIndex, HashMap<&'a NodeIndex, &'a Edge>> {
-		let mut edges = HashMap::<&NodeIndex, HashMap<&NodeIndex, &Edge>>::new();
+	pub fn edges_with_label<'a>(&'a self, label: &String) -> BTreeMap<&'a NodeIndex, BTreeMap<&'a NodeIndex, &'a Edge>> {
+		let mut edges = BTreeMap::<&NodeIndex, BTreeMap<&NodeIndex, &Edge>>::new();
 		for (src, map) in self.edges.iter() {
-			let filtered: HashMap<&NodeIndex, &Edge> = map.iter().filter(|&(_idx, edge)| edge.labels.contains(&label)).collect();
+			let filtered: BTreeMap<&NodeIndex, &Edge> = map.iter().filter(|&(_idx, edge)| edge.labels.contains(label)).collect();
 			if filtered.len() > 0 {
 				edges.insert(src, filtered);
 			}
@@ -180,15 +218,18 @@ impl Graph {
 	}
 
 	/// Get Nodes with a given label from a source NodeIndex
-	pub fn edges_with_label_from<'a>(&'a mut self, source: NodeIndex, label: ~str) -> ~[&'a Node] {
-		self.edges.get(&source).iter().filter_map(
-			|(&idx, edge)| 
+	pub fn edges_with_label_from<'a>(&'a self, source: NodeIndex, label: String) -> Vec<&'a Node> {
+		if let Some(edge) = self.edges.get(&source) {
+		edge.iter().filter_map(
+			|(&idx, edge)|
 				if edge.labels.contains(&label) {
-					self.nodes.find(&idx)
+					self.nodes.get(&idx)
 				} else {
 					None
 				}).collect()
-	} 
+			} else {
+				vec![]
+			}
+	}
 
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-#![feature(macro_rules)]
-
-extern crate collections;
-extern crate serialize;
+#![feature(convert)]
+extern crate rustc_serialize;
 // use std::io::net::tcp::TcpStream;
 // use std::io::net::ip::{Ipv4Addr, SocketAddr};
 use graph::Graph;
 use std::io;
+use std::io::BufRead;
 mod graph;
 
 
 // Shorthand HashMap
 // dict!({"yes": "1", "no": "0"}) => vec!($(($key, $value)),*).move_iter().collect();
-macro_rules! dict (
-	({$($key:expr : $value:expr),*}) => (vec!($(($key, $value)),*).move_iter().collect())
-)
+// macro_rules! dict (
+// 	({$($key:expr : $value:expr),*}) => (vec!($(($key, $value)),*).move_iter().collect())
+// );
 
 fn main() {
 	println!("Graphael 0.1");
@@ -21,21 +20,22 @@ fn main() {
 	print!("Enter a graph name> ");
 
 	// Get the graph file name from stdin or use 'langs'
-	let graph_file : ~str = match io::stdin().read_line() {
-		Ok(s) => {
-			println!("Using '{}'", &s.trim());
-			s.trim().to_owned()
+	let mut input = String::new();
+	let graph_file = match io::stdin().read_line(&mut input) {
+		Ok(_) => {
+			println!("Using '{}'", &input.trim());
+			input.trim().to_owned()
 		},
 		Err(e) => {
 			println!("Error: {}.\nUsing 'langs'", e);
-			~"langs"
+			"langs".to_string()
 		}
 	};
 
-	// Read a already filled database 
-	let mut graph = Graph::read_from_file(format!("./data/{}.graph", graph_file));
+	// Read a already filled database
+	let graph = Graph::read_from_file(format!("./data/{}.graph", graph_file));
 
-	// Current state variable to keep track of which 
+	// Current state variable to keep track of which
 	// type of query we are doing
 	let mut current_state = 0;
 
@@ -45,49 +45,50 @@ fn main() {
 	println!("4. Edges from node with label.");
 	println!("5. Look up node.");
 	println!("");
-	print!(">>> ")
+	print!(">>> ");
 
 	// Read from stdin
-	for line in io::stdin().lines() {
+	let locked_in = io::stdin();
+	for line in locked_in.lock().lines() {
 		match line {
 			Ok(s) => {
 				match current_state {
 					// Initial state
-					0 => match from_str::<int>(s.trim()) {
-						Some(1) => current_state = 1,
-						Some(2) => current_state = 2,
-						Some(3) => current_state = 3,
-						Some(4) => current_state = 4,
-						Some(5) => current_state = 5,
+					0 => match s.trim().parse::<i32>() {
+						Ok(1) => current_state = 1,
+						Ok(2) => current_state = 2,
+						Ok(3) => current_state = 3,
+						Ok(4) => current_state = 4,
+						Ok(5) => current_state = 5,
 						_ => current_state = 0
 					},
 					1 => { // Nodes with attribute
-						println!("{}", graph.nodes_with_attr(s.trim().to_owned()));
+						println!("{:?}", graph.nodes_with_attr(&s.trim().to_string()));
 						current_state = 0
 					},
 					2 => { // Node by key-value pair
-						let kv : ~[&str] = s.trim().split('=').map(|x| x.trim()).collect();
-						println!("{}", graph.nodes_with_prop(kv[0].to_owned(), kv[1].to_owned()));
+						let kv : Vec<&str> = s.trim().split('=').map(|x| x.trim()).collect();
+						println!("{:?}", graph.nodes_with_prop(&kv[0].to_string(), &kv[1].to_string()));
 						current_state = 0
 					},
 					3 => { // Edges with label
-						println!("{}", graph.edges_with_label(s.trim().to_owned()));
+						println!("{:?}", graph.edges_with_label(&s.trim().to_string()));
 						current_state = 0
 					},
 					4 => { // Edges from node (NodeIndex) with label
-						let node_label : ~[&str] = s.trim().split_str("HAS").map(|x| x.trim()).collect();
-						println!("{}", graph.edges_with_label_from(from_str::<uint>(node_label[0].to_owned()).unwrap(), node_label[1].to_owned()));
+						let node_label : Vec<String> = s.trim().split("HAS").map(|x| x.trim().to_string()).collect();
+						println!("{:?}", graph.edges_with_label_from((node_label[0].to_owned().parse::<usize>()).unwrap(), node_label[1].to_owned()));
 						current_state = 0
 					},
 					5 => { // Look up node by id (NodeIndex)
-						let nodeid = from_str::<uint>(s.trim()).unwrap();
-						println!("{}", graph.nodes.find(&nodeid));
+						let nodeid = (s.trim().parse::<usize>()).unwrap();
+						println!("{}", graph.nodes.get(&nodeid).unwrap());
 						current_state = 0
 					},
 					_ => { print!(">>> ") }
 				}
 			},
-			Err(e) => fail!("Error: {}", e)
+			Err(e) => panic!("Error: {}", e)
 		};
 
 		// Check the state and print accordingly
